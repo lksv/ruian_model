84e8d4bd86b058aa2fa0b1a3e00ddd7db50525c0
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-12-11 22:42:06 +0100

    fix spec

--- spec/ruian_model_spec.rb
+++ spec/ruian_model_spec.rb
@@ -12,10 +12,4 @@ describe RuianModel do
     expect(ActiveRecord::Base.configurations).not_to be_empty
     expect { defined? Obec.first }.to_not raise_error
   end
-
-  it '.load_models define load all model clases' do
-    expect(defined? Obec).to be_falsy
-    RuianModel.load_models
-    expect(defined? Obec).to be_truthy
-  end
 end
