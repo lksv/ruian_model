8aa785b9158c5950eb74b1b995a8f29366d8b90c
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-12-18 14:49:01 +0100

    feat(AdresniMisto): add scopes searching address

--- lib/ruian_model/adresni_misto.rb
+++ lib/ruian_model/adresni_misto.rb
@@ -32,4 +32,45 @@ class AdresniMisto < ActiveRecord::Base
   belongs_to :ulice, foreign_key: 'ulice_kod'
 
   delegate :hranice, :definicni_bod, to: :stavebni_objekt
+
+  scope :by_ku, ->(ku) {
+    joins(stavebni_objekt: {parcela: :katastralni_uzemi})
+      .where('rn_katastralni_uzemi.nazev IN (?)', Array.wrap(ku))
+  }
+
+  scope :by_obec, ->(obec) {
+    joins(stavebni_objekt: {parcela: {katastralni_uzemi: :obec}})
+      .where('rn_obec.nazev IN (?)', Array.wrap(obec))
+  }
+
+  scope :by_cast_obce, ->(cast_obce) {
+    joins(stavebni_objekt: :cast_obce )
+      .where('rn_cast_obce.nazev IN (?)', Array.wrap(cast_obce))
+  }
+
+  scope :by_momc, ->(momc) {
+    joins(stavebni_objekt: :momc)
+      .where('rn_momc.nazev IN (?)', momc)
+  }
+
+  scope :by_ulice, ->(ulice) {
+    joins(:ulice).where('rn_ulice.nazev IN (?)', Array.wrap(ulice))
+  }
+
+  scope :by_cislo_evidencni, ->(cislo_evidencni) {
+    joins(:stavebni_objekt).where(
+      cislo_domovni: cislo_evidencni,
+      rn_stavebni_objekt: { typ_kod: 2 }
+    )
+  }
+
+  scope :by_cislo, ->(cislo_popisne, cislo_orientacni_hodnota, cislo_orientacni_pismeno) {
+    res = all
+    res = res.joins(:stavebni_objekt).where(cislo_domovni: cislo_popisne, rn_stavebni_objekt: { typ_kod: 1}) if cislo_popisne.present?
+    res = res.where(cislo_orientacni_hodnota: cislo_orientacni_hodnota) if cislo_orientacni_hodnota.present?
+    res = res.where(cislo_orientacni_pismeno: cislo_orientacni_pismeno.present? ? cislo_orientacni_pismeno : nil) if cislo_orientacni_hodnota.present?
+    res
+  }
+
+
 end
